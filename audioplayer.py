fb5d92896c04a5f8353ae7c75933f0cb39739050
bellcha <bellcha@gmail.com>
2022-03-19 16:36:32 -0500

    I don't know

--- audioplayer.py
+++ audioplayer.py
@@ -135,7 +135,7 @@ def play(path):
         print (e)
 
 
-class Screen(object):
+class Screen:
     def __init__(self, item):
         self.item = item
 
@@ -300,7 +300,7 @@ def get_screen_for_bookmark(folder):
         return None
 
 
-class FileBrowser(object):
+class FileBrowser:
     def __init__(self, folder):
         self.folder = folder
         self.current = get_screen(folder)
